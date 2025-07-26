4e35e00525406790d4e2091ab3e01d3bfd300b0c
Dmitry Timushev <timushev@gmail.com>
2025-07-26 23:37:18 +0300

    add dockerfile

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
 import dotenv from "dotenv";
 import { router as routes } from "./routes/index.js";
 
@@ -8,10 +9,14 @@ dotenv.config();
 const port = 3001;
 const app = express();
 
-app.use(express.static("../frontend/dist"));
+app.use(express.static(path.resolve("..", "frontend", "dist")));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("..", "frontend", "dist", "index.html"));
+});
 
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
   app.listen(port, () => {
